19b31b9e62ec57fc3fbdfdd8c460f7e93612d177
P1x3lc0w <p1x3lc0w@protonmail.com>
2020-06-26 12:02:52 +0200

    Add StringExtensions

---
+++ P1x3lc0w.Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1x3lc0w.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string StripDiscordCodeBlockChars(this string str)
+            => str?.Replace("`", "´");
+    }
+}
